965eed3658ad1d691731e04a8d030de558475623
Caine Jette <caine@attackpattern.com>
2016-04-03 16:19:24 -0700

    Don't write out temporary expiration.txt file anymore.

--- src/actions/ExpirationDateChosenActionListener.java
+++ src/actions/ExpirationDateChosenActionListener.java
@@ -2,10 +2,6 @@ package actions;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
 import javax.swing.JDialog;
 
@@ -27,24 +23,8 @@ public class ExpirationDateChosenActionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
-		String expirationP = model.getYear() + "/" + (model.getMonth() + 1) + "/" + model.getDay();
-		
-		File expirationFile = new File("expiration.txt");
-		this.builder.setExpiration(expirationP);
-		BufferedWriter writer = null;
-		try {
-			writer = new BufferedWriter(new FileWriter(expirationFile));
-            writer.write(expirationP);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		} finally {
-			try {
-				writer.close();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-		}
+		String expiration = model.getYear() + "/" + (model.getMonth() + 1) + "/" + model.getDay();
+		this.builder.setExpiration(expiration);
 
 		dialog.setVisible(false);
 		dialog.dispose();
